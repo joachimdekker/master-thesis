ccf7259dc86cb0edfd1cbd07925a6220f170eee5
Joachim Dekker <joachim.dekker@infosupport.com>
2025-06-11 14:27:25 +0200

    feat: update `Chain` and `Table` structures with selections
    Added `Selection` properties for `Header`, `Data`, and `Footer` to both `Chain` and `Table` classes, improving structure representation. Adjusted `Columns` to align with new definitions, enabling better modeling of spreadsheet blocks.

--- ExcelCompiler.Representations/Structure/Specialized/Chain.cs
+++ ExcelCompiler.Representations/Structure/Specialized/Chain.cs
@@ -4,5 +4,14 @@ namespace ExcelCompiler.Representations.Structure;
 
 public class Chain : Construct
 {
+    
+    public string Header { get; init; }
+    
+    public Selection Initialisation { get; init; }
+    
+    public Selection Data { get; init; }
+    
+    public Selection Footer { get; init; }
+    
     public Dictionary<string, Range> Columns { get; init; } = [];
 }
--- ExcelCompiler.Representations/Structure/Specialized/Table.cs
+++ ExcelCompiler.Representations/Structure/Specialized/Table.cs
@@ -4,5 +4,11 @@ namespace ExcelCompiler.Representations.Structure;
 
 public class Table : Construct
 {
-    public Dictionary<string, Range> Columns { get; init; } = [];
+    public Selection? Header { get; init; }
+    
+    public Selection Data { get; init; }
+    
+    public Selection? Footer { get; init; }
+    
+    public Dictionary<string, Selection> Columns { get; init; } = [];
 }
