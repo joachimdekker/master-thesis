2179db304100f9fbb5207fc5523bd81c5500094e
Joachim Dekker <joachim.dekker@infosupport.com>
2025-07-04 09:41:36 +0200

    feat(program): add timer to measure the speed of the compiler

--- ExcelCompiler.Cli/ProjectCreationWorker.cs
+++ ExcelCompiler.Cli/ProjectCreationWorker.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics;
 using ExcelCompiler.Cli.Config;
 using ExcelCompiler.Generators;
 using ExcelCompiler.Representations.CodeLayout;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace ExcelCompiler.Cli;
@@ -10,11 +12,13 @@ public class ProjectCreationWorker
     private readonly ProjectGenerator _projectGenerator;
     private readonly RoslynGenerator _programGenerator;
     private readonly OutputConfiguration _configuration;
+    private readonly ILogger<ProjectCreationWorker> _logger;
 
-    public ProjectCreationWorker(ProjectGenerator projectGenerator, IOptions<OutputConfiguration> configuration, RoslynGenerator programGenerator)
+    public ProjectCreationWorker(ProjectGenerator projectGenerator, IOptions<OutputConfiguration> configuration, RoslynGenerator programGenerator, ILogger<ProjectCreationWorker> logger)
     {
         _projectGenerator = projectGenerator;
         _programGenerator = programGenerator;
+        _logger = logger;
         _configuration = configuration.Value;
     }
     
@@ -26,10 +30,17 @@ public class ProjectCreationWorker
         }
         
         // Create the project
+        Stopwatch sw = new();
+        sw.Start();
+        
         DirectoryInfo directory = new DirectoryInfo(_configuration.Location);
         await _projectGenerator.Generate(directory);
         
         // Create the main file
         await _programGenerator.Generate(project, _configuration.Location);
+
+        sw.Stop();
+        
+        _logger.LogInformation("Project generation took {Time} ms", sw.ElapsedMilliseconds);
     }
 }
