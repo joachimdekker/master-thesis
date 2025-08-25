dbadcabf113b9ce10329a0efd28dd32382b2ff39
Joachim Dekker <joachim.dekker@infosupport.com>
2025-08-25 10:35:55 +0200

    refactor(timer): simplify stopwatch usage and fix time display
    Replaced redundant `TimeSpan` variable with `Stopwatch.Elapsed` for simplicity and clarity. Adjusted time computation logic to use `Stopwatch.Elapsed` directly, ensuring accurate time measurements are displayed. Cleaned up related code for improved readability and maintainability.

--- ExcelCompiler.GeneratedProjectRunner/Program.cs
+++ ExcelCompiler.GeneratedProjectRunner/Program.cs
@@ -6,7 +6,6 @@ Stopwatch sw = new();
 Stopwatch outerSw = new();
 
 double total = 0;
-TimeSpan time = TimeSpan.Zero;
 
 double[] inputs = [0.234d, 0.015d, 0.002567d, 0.30d, 0.003652d, 0.05d];
 int count = 1_000_000;
@@ -16,17 +15,16 @@ outerSw.Start();
 for (int i = 0; i < count; i++)
 {
     var input = inputs[i % inputs.Length];
-    sw.Restart();
+    sw.Start();
     var output = program.Main();
     sw.Stop();
     
     total += output;
-    time += sw.Elapsed;
 }
 outerSw.Stop();
 
-Console.WriteLine($"Total time: {time.TotalMilliseconds} ms");
-Console.WriteLine($"Average time: {time.TotalMilliseconds / count} ms");
+Console.WriteLine($"Total time: {sw.Elapsed.TotalMilliseconds} ms");
+Console.WriteLine($"Average time: {sw.Elapsed.TotalMilliseconds / count} ms");
 Console.WriteLine($"Total output: {total}");
 Console.WriteLine($"Average output: {total / count}");
 Console.WriteLine($"Walltime for {count} iterations: {outerSw.Elapsed.TotalSeconds} s / {outerSw.Elapsed.TotalMilliseconds} ms");
